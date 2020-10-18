6b5d969f503e676b68ed2283815e294389405b04
Gustavo Toro <gustavo@torocaballero.com>
2020-10-18 10:39:39 -0400

    Ajuste el texto en las fotos

--- Main.js
+++ Main.js
@@ -1,4 +1,5 @@
 var slideIndex = 1;
+AutoshowSlides();
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -26,3 +27,16 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function AutoshowSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(AutoshowSlides, 5000); // Change image every 2 seconds
+  }
+  
